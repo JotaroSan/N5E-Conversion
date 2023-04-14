c199b771a7f57f0d85c38b057f0c1e7d29023336
Jordan Nelson <jwood13131313@gmail.com>
2023-04-14 18:34:52 -0400

    Updated the ReadMe to be more up to date on changes actually being provided

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # N5E-Conversion
 N5E is a Foundry Add-On Module to convert the standard DND5E to the Naruto Based N5E version
 
+## What This Module Actually Does
+1. Add new damage types (Chakra, Wind, Earth) and resistance types (Chakra, Wind, Earth).
+2. Add new spell components (Hand Seals, Chakra Molding, Chakra Seals, Mobility, Ninja Tools, Weapons, Fuinjutsu, Medical, and Clash).
+3. Add a new sense (Chakra Sight).
+4. Add new skills (Ninshou, Martial Arts, Illusion, and Chakra Control).
+5. Add a new ability activation type (Full Turn Action).
+6. Change the proficiency formula calculation.
+7. Remove Nature, Religion, and Arcana skills.
+8. Remove Verbal and Ritual spell components.
+9. Remove spell level options 6, 7, 8, and 9.
+10. Remove Spell Schools Divination, Transmutation, and Enchantment. 
+11. The personality trait is now "Ambition."
+12. The inspiration is now "Will of Fire."
+13. The flaws are now "Fears."
+14. The bonds are now "Goals."
+15. The ideals are now "Drive."
+16. The currency abbreviation is now "ryo."
+17. The currency name is now "Ryo."
+18. All instance of "Spell" are now refered to as "Jutsu"
+18. The abjuration school of magic is now "Hijutsu."
+19. The conjuration school of magic is now "Taijutsu."
+20. The evocation school of magic is now "Ninjutsu."
+21. The illusion school of magic is now "Genjutsu."
+22. The necromancy school of magic is now "Bukijutsu."
+
+## Required 
+- Foundry V10
+- DND5E System Installed
+
+## STRONGLY RECCOMENDED
+- Folder Compendium Mod
---
+++ create_release.py
@@ -0,0 +1,37 @@
+import json, os, argparse, zipfile
+
+# The name of the GitLab user/group that the repo is owned by, for the download URL
+gitlab_group = 'JotaroSan'
+
+# The individual files to include in the repo
+include_files = ['module.json','n5econversion.mjs','LICENSE','n5econversion.css']
+
+# The folders
+include_folders = ['lang','templates','packs']
+
+# -----------------------------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('package_type', help="The package type, generally 'system' or 'module'")
+parser.add_argument('release_tag', help="The version to specify in the manifest, generally the GitLab tag for the release")
+args = parser.parse_args()
+
+# Open the existing manifest file and determine the eventual zip name with it
+manifest = json.load(open(f'{args.package_type}.json','r'))
+zip_filename = f"{manifest['id']}-{args.release_tag}.zip"
+
+# Write the version and download link to the manifest
+manifest['version'] = str(args.release_tag)
+#manifest['manifest'] = f"https://gitlab.com/{gitlab_group}/{manifest['id']}/-/releases/permalink/latest/downloads/{args.package_type}.json"
+manifest['download'] = f"https://gitlab.com/{gitlab_group}/{manifest['id']}/-/releases/{args.release_tag}/downloads/{zip_filename}"
+json.dump(manifest, open(f'{args.package_type}.json', 'w'), indent=2)
+
+
+# Add all of the specified files to the final zip that's being output
+with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+    for filename in include_files:
+        zip_file.write(filename)
+
+    for folder_name in include_folders:
+        for path,subfolders,files in os.walk(folder_name):
+            for filename in files:
+                zip_file.write(os.path.join(path,filename))
