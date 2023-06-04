b1e9212c8248cf0d4810e79a4eae48a24b0aecfb
Jordan Nelson <jwood13131313@gmail.com>
2023-06-04 10:32:12 -0400

    Cleanup Logic on Jutsu Wrote Python Script to check for level, rank and error proofing. Read Only. Testing to Come

--- create_release.py
+++ create_release.py
@@ -4,27 +4,30 @@ import json, os, argparse, zipfile
 gitlab_group = 'JotaroSan'
 
 # The individual files to include in the repo
-include_files = ['module.json','n5econversion.mjs','LICENSE','n5econversion.css']
+include_files = ['module.json', 'n5econversion.mjs', 'LICENSE', 'n5econversion.css']
 
 # The folders
-include_folders = ['lang','templates','packs']
+include_folders = ['lang', 'templates', 'packs', 'icons', 'scripts', 'styles']
 
 # -----------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument('package_type', help="The package type, generally 'system' or 'module'")
-parser.add_argument('release_tag', help="The version to specify in the manifest, generally the GitLab tag for the release")
+parser.add_argument('release_tag',
+                    help="The version to specify in the manifest, generally the GitLab tag for the release")
 args = parser.parse_args()
 
 # Open the existing manifest file and determine the eventual zip name with it
-#manifest = json.load(open(f'{args.package_type}.json','r'))
-#zip_filename = f"{manifest['id']}-{args.release_tag}.zip"
+manifest = json.load(open('{}.json'.format(args.package_type), 'r'))
+zip_filename = "{}-{}.zip".format(manifest['id'], args.release_tag)
 
 # Write the version and download link to the manifest
-#manifest['version'] = str(args.release_tag)
-#manifest['manifest'] = f"https://gitlab.com/{gitlab_group}/{manifest['id']}/-/releases/permalink/latest/downloads/{args.package_type}.json"
-#manifest['download'] = f"https://gitlab.com/{gitlab_group}/{manifest['id']}/-/releases/{args.release_tag}/downloads/{zip_filename}"
-#json.dump(manifest, open(f'{args.package_type}.json', 'w'), indent=2)
+manifest['version'] = str(args.release_tag)
+manifest['manifest'] = "https://gitlab.com/{}/-/releases/permalink/latest/downloads/{}.json".format(gitlab_group,
+                                                                                                    manifest['id'])
+manifest['download'] = "https://gitlab.com/{}/-/releases/{}/downloads/{}".format(gitlab_group, args.release_tag,
+                                                                                 zip_filename)
 
+json.dump(manifest, open('{}.json'.format(args.package_type), 'w'), indent=2)
 
 # Add of the specified files to the final zip that's being output
 with zipfile.ZipFile(zip_filename, 'w') as zip_file:
@@ -32,6 +35,6 @@ with zipfile.ZipFile(zip_filename, 'w') as zip_file:
         zip_file.write(filename)
 
     for folder_name in include_folders:
-        for path,subfolders,files in os.walk(folder_name):
+        for path, subfolders, files in os.walk(folder_name):
             for filename in files:
-                zip_file.write(os.path.join(path,filename))
+                zip_file.write(os.path.join(path, filename))
