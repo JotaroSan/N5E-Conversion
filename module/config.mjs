b1594325bfac85be1bb572df8a996aa41bb496e1
Jordan Nelson <jwood13131313@gmail.com>
2023-11-29 16:30:06 -0500

    Made some Modifications to config and properties being added. Doing a full revision of project.

---
+++ module/config.mjs
@@ -0,0 +1,174 @@
+const N5ECONFIG = {
+  DND5E: {},
+  N5E: {},
+};
+
+/* Add N5E Damage Types */
+N5ECONFIG.DND5E.damageTypes = {
+  chakra: "Chakra",
+  wind: "Wind",
+  earth: "Earth",
+};
+/* Add N5E Spell Components */
+N5ECONFIG.DND5E.spellComponents = {
+  handseals: { label: "Hand Seals", abbr: "HS" },
+  molding: { label: "Chakra Molding", abbr: "CM" },
+  chakraseals: { label: "Chakra Seals", abbr: "CS" },
+  mobility: { label: "Mobility", abbr: "M" },
+  ninjatools: { label: "Ninja Tools", abbr: "NT" },
+  weapons: { label: "Weapons", abbr: "W" },
+  fuinjutsu: { label: "Fuinjutsu", abbr: "F" },
+  medical: { label: "Medical", abbr: "Med" },
+  clash: { label: "Clash", abbr: "CL" },
+};
+/* Add N5E Senses */
+N5ECONFIG.DND5E.senses = {
+  "chakra sight": "Chakra sight",
+};
+/* Add N5E Weapon IDs */
+N5ECONFIG.DND5E.weaponIds = {
+  battlewire: "n5econversion.n5e-items.81zHkZgJJUswAl8C",
+  blowgun: "n5econversion.n5e-items.ZdkI4f6cAvCaSDc1",
+  handaxe: "n5econversion.n5e-items.GUsi2ly8gxYK0TH2",
+  chainedhandscythe: "n5econversion.n5e-items.M8tl0zQAH18FxBLo",
+  handscythe: "n5econversion.n5e-items.X1dlx00oTkwMp6ei",
+  kunai: "n5econversion.n5e-items.RbcM59vdQ9ZgYBKV",
+  quarterstaff: "n5econversion.n5e-items.XAVJqdQWLqV8jffm",
+  spear: "n5econversion.n5e-items.Qrxp2ypJNrdBfnxF",
+  tanto: "n5econversion.n5e-items.HbrlOL6HWNiuNaiU",
+  weightedchain: "n5econversion.n5e-items.MVeoq1E1DCwCZTwc",
+  senbon: "n5econversion.n5e-items.AqeyHEGdWCFAqWP0",
+  shortbow: "n5econversion.n5e-items.1vdllonFUUQnkUvJ",
+  shuriken: "n5econversion.n5e-items.YReiEBmBtKhLaoxF",
+  sling: "n5econversion.n5e-items.yNkLtAbQTIZY7ZCC",
+  broadsword: "n5econversion.n5e-items.xe110rLxmQETPJyq",
+  chainedspear: "n5econversion.n5e-items.PTHvIz6IH9HpbHg7",
+  combatbracers: "n5econversion.n5e-items.7JdFwOiRWfq35OWT",
+  greataxe: "n5econversion.n5e-items.5xpZcxYjWmAjXo1j",
+  hookedlance: "n5econversion.n5e-items.bEtYwzE76RhPISms",
+  ironclaw: "n5econversion.n5e-items.00uRBzVYYgEZFy4Q",
+  jitte: "n5econversion.n5e-items.M5abrR0syMtZh0Xx",
+  katana: "n5econversion.n5e-items.h9iLNkcRhCnDZGAO",
+  naginata: "n5econversion.n5e-items.5Ay4NnbFHXHmFTi0",
+  nunchaku: "n5econversion.n5e-items.14f6YrXLHRNXJXzA",
+  odachi: "n5econversion.n5e-items.SMFAFXkdTIvipYqB",
+  sasumata: "n5econversion.n5e-items.pc6Z8vmwn4TKyXya",
+  scythe: "n5econversion.n5e-items.AEHqJwlyfAPCGPgX",
+  tetsubo: "n5econversion.n5e-items.mlxyNFZW5tPGsb0R",
+  tonfa: "n5econversion.n5e-items.AxFRDY4CRyUOEoLo",
+  warclub: "n5econversion.n5e-items.lN8v13BroQh5YgI5",
+  whip: "n5econversion.n5e-items.W5ZfgVCgxsGpLzR4",
+  knuckleblade: "n5econversion.n5e-items.VSbOUoRmdTYpBPaS",
+  chakram: "n5econversion.n5e-items.L1AT9ekTDUJQHB9T",
+  monsterchakram: "n5econversion.n5e-items.nMbckfVmQyomfZY3",
+  longbow: "n5econversion.n5e-items.ZTgwdJyugEGiMJqu",
+  crossbowhand: "n5econversion.n5e-items.7efcoLqgvay98RUV",
+  crossbowheavy: "n5econversion.n5e-items.OU67WU4IBvI5Uszm",
+  net: "n5econversion.n5e-items.0gs6UCdBaKZ5AIMl",
+  fumashuriken: "n5econversion.n5e-items.TXvRdUiQQuxwTgyF",
+  monstershuriken: "n5econversion.n5e-items.sxBvPLkkop5jkScv",
+};
+/* Add N5E Armor IDs */
+N5ECONFIG.DND5E.armorIds = {
+  padded: "n5econversion.n5e-items.PXA5xkWhB5D1y2ZL",
+  leather: "n5econversion.n5e-items.wr07gpjgWZQw2NgC",
+  studded: "n5econversion.n5e-items.zHootXIbZBSCHawn",
+  combat: "n5econversion.n5e-items.II33J57LVqZSqnW2",
+  armored: "n5econversion.n5e-items.LfNh1vvFhiIFlKdg",
+  chunin: "n5econversion.n5e-items.ycaJ6PMpeLn8Ymw7",
+  reinforced: "n5econversion.n5e-items.oUeGPcPifVZsUVrv",
+  flak: "n5econversion.n5e-items.lY4pmzqaXsy7c0Vy",
+  armorflak: "n5econversion.n5e-items.I7yXden0O9KSv7tk",
+  battle: "n5econversion.n5e-items.Rc8XgYyUD3Vd9XU1",
+  shinobi: "n5econversion.n5e-items.90v6et8pq48v72O5",
+  jonin: "n5econversion.n5e-items.A6gXO1rI6UWUQnJ9",
+  armoredjonin: "n5econversion.n5e-items.WFkedOjW63ZeVCDo",
+  ronin: "n5econversion.n5e-items.0g3HQc6zT0GmYoYj",
+  samurai: "n5econversion.n5e-items.1ylLZzKoatc5kybx",
+};
+/* Add N5E Armor Classes */
+N5ECONFIG.DND5E.armorClasses = {
+  default: { label: "Skin", formula: "10 + floor(@prof/2) + @abilities.dex.mod" },
+  shark: { label: "Shark Skin", formula: "10 + floor(@prof/2) + @abilities.con.mod" },
+  psycha: { label: "Psy Cha", formula: "10 + floor(@prof/2) + @abilities.cha.mod" },
+  psywis: { label: "Psy Wis", formula: "10 + floor(@prof/2) + @abilities.wis.mod" },
+  evasive: { label: "Evasive", formula: "10 + @prof + @abilities.dex.mod" },
+  geo: { label: "Geo", formula: "13 + floor(@prof/2) + @abilities.con.mod" },
+  sharingan: { label: "Sharingan", formula: "10 + floor(@prof/2) + @abilities.dex.mod + @abilities.int.mod" },
+  bone: { label: "Bone", formula: "10 + floor(@prof/2) + @abilities.dex.mod + @abilities.con.mod" },
+  unarmored: { label: "Martial Defense", formula: "10 + @prof + @abilities.dex.mod + @abilities.wis.mod" },
+  custom: { label: "Custom" },
+};
+/* Add N5E Status Effects */
+N5ECONFIG.DND5E.statusEffects = [
+  { id: "burned1", label: "Burned [1]", icon: "modules/n5econversion/icons/conditions/Burned-1.png" }
+];
+/* Add N5E Weapon Properties */
+N5ECONFIG.DND5E.weaponProperties = {
+  amm: "Ammunition",
+  blk: "Blocking",
+  btw: "Battle Wire",
+  crit: "Critical",
+  dly: "Deadly",
+  dis: "Disarm",
+  evo: "Evocation",
+  fle: "Flexible",
+  fin: "Finesse",
+  grp: "Grapple",
+  hvy: "Heavy",
+  hid: "Hidden",
+  lth: "Lethal",
+  lgt: "Light",
+  ldg: "Loading",
+  ma: "Multiattack",
+  rng: "Range",
+  rch: "Reach",
+  ret: "Returning",
+  tac: "Tactical",
+  thn: "Thrown",
+  tri: "Trip",
+  th: "Two-handed",
+  ua: "Unarmed",
+  ver: "Versatile",
+  vol: "Volatile",
+  win: "Winding"
+};
+/* Add N5E Spell Schools */
+N5ECONFIG.DND5E.spellSchools = {
+  nin: "Ninjutsu",
+  tai: "Taijutsu",
+  buki: "Bukijutsu",
+  gen: "Genjutsu",
+  hij: "Hijutsu",
+};
+/* Add N5E Healing Types */
+N5ECONFIG.DND5E.healingTypes = {
+  chakra: "Chakra",
+  tempchakra: "Temp Chakra",
+};
+/* Add N5E Skills */
+N5ECONFIG.DND5E.skills = {
+  nin: { label: "Ninshou", ability: "int" },
+  cft: { label: "Crafting", ability: "int" },
+  mar: { label: "Martial Arts", ability: "str" },
+  ill: { label: "Illusion", ability: "wis" },
+  ccon: { label: "Chakra Control", ability: "con" },
+};
+/* Add N5E ActivationTypes */
+N5ECONFIG.DND5E.abilityActivationTypes = {
+  fta: "Full Turn Action",
+};
+/* Add N5E Languages */
+N5ECONFIG.DND5E.languages = {
+  low: "Land of Water Dialect",
+  lof: "Land of Fire Dialect",
+  loe: "Land of Earth Dialect",
+  loc: "Land of Cloud Dialect",
+  los: "Land of Sand Dialect",
+  snk: "Snake",
+  dog: "Dog",
+  Ins: "Insect",
+};
+
+
+export default N5ECONFIG;
---
+++ n5econversion.mjs
@@ -0,0 +1,156 @@
+/* N5E Conversion Module */
+
+/* Import Files */
+import N5ECONFIG from "./module/config.mjs";
+
+/* Set Constants */
+const moduleID = "n5econversion";
+
+/* Hook & Merge N5E Config */
+Hooks.once("init", () => {
+    CONFIG.DND5E.weaponProperties = {};
+    CONFIG.DND5E.armorClasses = {};
+    CONFIG.DND5E.armorIds = {};
+    CONFIG.DND5E.weaponIds = {};
+    console.log('N5E Module Object Merge');
+    foundry.utils.mergeObject(CONFIG, N5ECONFIG);
+});
+
+/*Proficiency Wrapper Modifies Level being ingest to produce correct proficiency mod for N5E system */
+Hooks.once('init', async function() {
+  libWrapper.register(moduleID, 'dnd5e.documents.Proficiency.calculateMod', function (wrapped, lvl) {
+    const level = lvl;
+	var newMod = 3;
+	if (level >= 0 && level < 4) {
+		newMod =  5;
+	  } else if (level >= 4 && level <= 6) {
+		newMod =  9;
+	  } else if (level >= 7 && level <= 9) {
+		newMod =  13;
+	  } else if (level >= 10 && level <= 12) {
+		newMod =  17;
+	  } else if (level >= 13 && level <= 15) {
+		newMod =  21;
+	  } else if (level >= 16 && level <= 18) {
+		newMod =  25;
+	  } else if (level >= 19 && level <= 20) {
+		newMod =  29;
+	  }
+    return wrapped(newMod);
+  }, 'WRAPPER');
+});
+
+
+// Remove Things that are no longer needed
+Hooks.on("setup", function () {
+  // Remove Nature, Religion and Arcana skills
+  CONFIG.DND5E.skills = Object.fromEntries(
+    Object.entries(CONFIG.DND5E.skills).filter(([key, value]) => !['arc', 'rel'].includes(key))
+  );
+  // Remove Verbal and Ritual spell components
+  CONFIG.DND5E.spellComponents = Object.fromEntries(
+    Object.entries(CONFIG.DND5E.spellComponents).filter(([key, value]) => !['vocal', 'somatic','material','ritual'].includes(key))
+  );
+  // Remove spell level options 6, 7, 8, and 9
+  CONFIG.DND5E.spellLevels = Object.fromEntries(
+    Object.entries(CONFIG.DND5E.spellLevels).filter(([key, value]) => !['6', '7', '8', '9'].includes(key))
+  );
+  // Remove Spell Schools
+  CONFIG.DND5E.spellSchools = Object.fromEntries(
+    Object.entries(CONFIG.DND5E.spellSchools).filter(([key, value]) => !['div', 'trs', 'enc','abj','evo','ill','nec','con'].includes(key))
+  );
+  // Remove Unnecessary Currencies
+  CONFIG.DND5E.currencies = Object.fromEntries(
+    Object.entries(CONFIG.DND5E.currencies).filter(([key, value]) => !['pp', 'ep', 'cp','sp'].includes(key))
+  );
+});
+
+// Inject resources onto sheet.
+Hooks.on("renderActorSheet", async function(sheet, html) {
+  sheet.document.setFlag("n5econversion", "resource.chakra", { label: 'Chakra', temp: 0, tempmax: 0 });
+  sheet.document.setFlag("n5econversion", "resource.chakraDice", { label: 'Chakra Dice', temp: 0, tempmax: 0  });
+  if (sheet.document.type !== "character") return;
+  const box = html[0].querySelector(".dnd5e.sheet.actor .header-details ul.attributes");
+  const div = document.createElement("DIV");
+  const resources = Object.entries(sheet.document.flags.n5econversion.resource ?? {}).reduce((acc, [id, data]) => {
+    if (!id) return acc;
+    acc.push({
+      label: (data.label || "").trim(),
+      name: `flags.n5econversion.resource.${id}`,
+      id,
+      temp: data.temp,
+      tempmax: data.tempmax,
+      value: data.value || null,
+      max: data.max || null
+    });
+    return acc;
+  }, []);
+  const template = "modules/n5econversion/templates/resource.hbs";
+  div.innerHTML = await renderTemplate(template, {resources});
+
+   div.querySelectorAll("input[type='text'][data-dtype='Number']").forEach(input => {
+     input.addEventListener("change", sheet._onChangeInputDelta.bind(sheet));
+   });
+
+   const foc = div.querySelector(`[name="${sheet._addarFocus}"]`);
+   box.append(...div.children);
+   if (foc && sheet._addarFocus.includes("addar")) foc.focus();
+
+   html[0].querySelectorAll("input").forEach(input => input.addEventListener("focus", (event) => {
+     sheet._addarFocus = event.currentTarget.name;
+     if (event.currentTarget.closest(".attributes")) event.currentTarget.select();
+   }));
+
+});
+
+/* Inject custom resource consumption option onto item sheet. */
+Hooks.on("renderItemSheet", function(sheet, html) {
+  if (sheet.item.system.consume?.type !== "flags.n5econversion.resource") return;
+  const actor = sheet.item.actor;
+  const ids = Object.keys(actor?.flags.n5econversion?.resource ?? {});
+  if (!ids.length) return;
+  const selected = sheet.item.system.consume.target;
+  const options = ids.reduce((acc, id) => {
+    const label = actor.flags.n5econversion.resource[id].label || game.i18n.localize("n5econversion.Resource");
+    const value = `flags.n5econversion.resource.${id}.value`;
+    const s = (value === selected) ? "selected" : "";
+    return acc + `<option value="${value}" ${s}>${label}</option>`;
+  }, "<option value=''></option>");
+  const tar = html[0].querySelector("[name='system.consume.target']");
+  if (tar) tar.innerHTML = options;
+});
+
+/* Adjust consumed target during item usage. */
+Hooks.on("dnd5e.preItemUsageConsumption", function(item, options, config) {
+  if (options.consumeResource && item.system.consume.target.startsWith("flags.n5econversion.resource")) {
+    options.consumeResource = false;
+    options.consumeCustomResource = item.system.consume.target;
+  }
+});
+
+/* Restore Resource on LR */
+Hooks.on("dnd5e.preRestCompleted", (actor, result) => {
+  if (!result.longRest) return true;
+  const chakra = actor.getFlag(moduleID, "chakra");
+  const chakraDice = actor.getFlag(moduleID, "chakraDice");
+  if (!chakra) return;
+  result.updateData["flags.prime-psionics.pp.value"] = pp.max;
+  result.updateData["flags.prime-psionics.pp.value"] = pp.max;
+});
+
+
+/* Adjust consumed target during item usage. */
+Hooks.on("dnd5e.itemUsageConsumption", function(item, options, config, updates) {
+  const name = options.consumeCustomResource;
+  if (!name) return;
+  const newValue = foundry.utils.getProperty(item.actor, name) - (item.system.consume.amount || 1);
+  if (newValue < 0) {
+    ui.notifications.warn(game.i18n.format("DND5E.ConsumeWarningNoQuantity", {
+      name: item.name,
+      type: CONFIG.DND5E.abilityConsumptionTypes[item.system.consume.type]
+    }));
+    return false;
+  }
+  updates.actorUpdates[name] = newValue;
+});
+
